f2f2f34f23678612e47ff544942d21176320d106
EvertonTrennepohl <gauchotp@gmail.com>
2022-03-09 17:45:38 -0300

    Update environment

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 import { AuthService } from '../authentication/auth.service';
 import { TokenStorageService } from '../authentication/token-storage.service';
